c5b52c369156fdf533dd77d881396416bbb1b79c
chi zhang <czchizhang@gmail.com>
2021-06-03 15:23:21 +0100

    updated lambda policy

--- lambda.tf
+++ lambda.tf
@@ -43,7 +43,7 @@ resource "aws_iam_policy" "s3_write_policy" {
               "s3:PutObjectAcl",
               "s3:PutLifecycleConfiguration"
         ],
-        "Resource": "${aws_s3_bucket.upload_bucket.arn}"
+        "Resource": "*"
     }
 ]
 
