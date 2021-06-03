05d966129e421169532fc043914e3e4d3ed7d5be
chi zhang <czchizhang@gmail.com>
2021-06-03 15:44:07 +0100

    Added API gateway configuration

--- api_gateway.tf
+++ api_gateway.tf
@@ -26,6 +26,23 @@ resource "aws_api_gateway_integration" "lambda" {
   uri                     = aws_lambda_function.s3_uploader.invoke_arn
 }
 
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.image_uploader.id
+  resource_id   = aws_api_gateway_rest_api.image_uploader.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.image_uploader.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.s3_uploader.invoke_arn
+}
+
 resource "aws_lambda_permission" "api_gateway" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
