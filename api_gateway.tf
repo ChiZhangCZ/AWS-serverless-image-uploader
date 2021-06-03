ecedf2d84f9bd6804f67f4de9b819b534da676df
chi zhang <czchizhang@gmail.com>
2021-06-03 15:10:01 +0100

    Added Lambda and API Gateway Terraform config

---
+++ api_gateway.tf
@@ -0,0 +1,38 @@
+resource "aws_api_gateway_rest_api" "image_uploader" {
+  name        = "Serverless Image Uploader"
+  description = "Serverless Image Uploader Gateway"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.image_uploader.id
+  parent_id   = aws_api_gateway_rest_api.image_uploader.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.image_uploader.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.image_uploader.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.s3_uploader.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_uploader.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.image_uploader.execution_arn}/*/*"
+}
---
+++ lambda.tf
@@ -0,0 +1,57 @@
+resource "aws_lambda_function" "s3_uploader" {
+  function_name = "s3Uploader"
+  handler = "app.handler"
+  role = aws_iam_role.lambda_exec.arn
+  runtime = "nodejs12.x"
+  filename = "getSignedURL/getSignedURL.zip"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_example_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_policy" "s3_write_policy" {
+  name = "s3_write_policy"
+  description = "Allows Lambda to write to S3"
+
+
+  policy = <<EOF
+   {
+"Version": "2012-10-17",
+"Statement": [
+    {
+        "Effect": "Allow",
+        "Action": [
+              "s3:PutObject",
+              "s3:PutObjectAcl",
+              "s3:PutLifecycleConfiguration"
+        ],
+        "Resource": "${aws_s3_bucket.upload_bucket.arn}"
+    }
+]
+
+}
+    EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.s3_write_policy.arn
+}
