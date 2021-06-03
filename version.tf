49bf51a85710b9cd8e32762efa9bfac9adb11aea
chi zhang <czchizhang@gmail.com>
2021-06-03 13:44:03 +0100

    fixed AWS provider version fixing

--- version.tf
+++ version.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/terraform"
-      version = "3.43.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.43.0"
     }
   }
 }
