012449700773e7d01585d4ca15220ccb01f9bc17
chi zhang <czchizhang@gmail.com>
2021-06-04 10:24:37 +0100

    blocked public access from s3 bucket

--- s3.tf
+++ s3.tf
@@ -18,3 +18,12 @@ resource "aws_s3_bucket" "upload_bucket" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket                  = aws_s3_bucket.upload_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
